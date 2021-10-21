54fae9e9f13753758ba607496978d7128ba938bb
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-10-21 13:39:36 +0200

    Método empezarJuego creado. Posición corregida TextNRondas en activity_main.xml

--- app/src/main/java/com/diego/simondice/MainActivity.kt
+++ app/src/main/java/com/diego/simondice/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.diego.simondice
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.os.HandlerCompat.postDelayed
 
 class MainActivity : AppCompatActivity() {
@@ -33,15 +39,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         val botonJugar: Button = findViewById(R.id.botonJugar)
+        botonJugar.setVisibility(View.VISIBLE)
         botonJugar.setOnClickListener() {
+            botonJugar.setVisibility(View.GONE)
             Toast.makeText(applicationContext, "3", Toast.LENGTH_SHORT).show()
             val handler = Handler()
             handler.postDelayed({
                 Toast.makeText(applicationContext, "2", Toast.LENGTH_SHORT).show()
-            }, 1000) //1 segundo
-            handler.postDelayed({
-                Toast.makeText(applicationContext, "1", Toast.LENGTH_SHORT).show()
-            }, 2000)
+                }, 1000) //1 segundo
+                handler.postDelayed({
+                    Toast.makeText(applicationContext, "1", Toast.LENGTH_SHORT).show()
+                }, 2000)
+            empezarJuego()
         }
+    }
+
+    private fun empezarJuego() {
+        val botonSuperior: Button = findViewById(R.id.botonSuperior)
+        val textoRondas: TextView = findViewById(R.id.textNRondas)
+        val handler = Handler()
+
+        handler.postDelayed({
+            for (i in 1..3) {
+                textoRondas.text= i.toString()
+                botonSuperior.background.setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP)
+            }
+        }, 3000)
     }
 }
