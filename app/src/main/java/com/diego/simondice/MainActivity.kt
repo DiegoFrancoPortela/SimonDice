43de0be0d0550554c0890cdc79f613e1e01449d5
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-10-21 14:36:57 +0200

    Cambio de colores

--- app/src/main/java/com/diego/simondice/MainActivity.kt
+++ app/src/main/java/com/diego/simondice/MainActivity.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.os.HandlerCompat.postDelayed
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,29 +42,63 @@ class MainActivity : AppCompatActivity() {
         val botonJugar: Button = findViewById(R.id.botonJugar)
         botonJugar.setVisibility(View.VISIBLE)
         botonJugar.setOnClickListener() {
+
             botonJugar.setVisibility(View.GONE)
-            Toast.makeText(applicationContext, "3", Toast.LENGTH_SHORT).show()
-            val handler = Handler()
-            handler.postDelayed({
-                Toast.makeText(applicationContext, "2", Toast.LENGTH_SHORT).show()
-                }, 1000) //1 segundo
-                handler.postDelayed({
-                    Toast.makeText(applicationContext, "1", Toast.LENGTH_SHORT).show()
-                }, 2000)
             empezarJuego()
         }
     }
 
     private fun empezarJuego() {
+        val botonJugar: Button = findViewById(R.id.botonJugar)
         val botonSuperior: Button = findViewById(R.id.botonSuperior)
-        val textoRondas: TextView = findViewById(R.id.textNRondas)
-        val handler = Handler()
+        val botonInferior: Button = findViewById(R.id.botonInferior)
+        val botonIzquierda: Button = findViewById(R.id.botonIzquierda)
+        val botonDerecha: Button = findViewById(R.id.botonDerecha)
 
-        handler.postDelayed({
-            for (i in 1..3) {
-                textoRondas.text= i.toString()
-                botonSuperior.background.setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP)
-            }
-        }, 3000)
+        val textoRondas: TextView = findViewById(R.id.textNRondas)
+
+        fun hiloJuego() {
+            val thread = Thread(Runnable {
+                for (i in 1..1) {
+                    textoRondas.text= i.toString()
+                    for (e in 1..4) {
+                        val shuffled = (1..4).shuffled().last()
+
+                        fun funcion1() {
+                            botonSuperior.background.setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP)
+                            Thread.sleep(1000)
+                            botonSuperior.background.setColorFilter(Color.parseColor("#ff0000"), PorterDuff.Mode.SRC_ATOP)
+                        }
+                        fun funcion2() {
+                            botonInferior.background.setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP)
+                            Thread.sleep(1000)
+                            botonInferior.background.setColorFilter(Color.parseColor("#800080"), PorterDuff.Mode.SRC_ATOP)
+                        }
+                        fun funcion3(){
+                            botonDerecha.background.setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP)
+                            Thread.sleep(1000)
+                            botonDerecha.background.setColorFilter(Color.parseColor("#008000"), PorterDuff.Mode.SRC_ATOP)
+                        }
+                        fun funcion4(){
+                            botonIzquierda.background.setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP)
+                            Thread.sleep(1000)
+                            botonIzquierda.background.setColorFilter(Color.parseColor("#0000FF"), PorterDuff.Mode.SRC_ATOP)
+                        }
+
+                        fun iniciarAleatorio() = when (shuffled) {
+                            1 -> funcion1()
+                            2 -> funcion2()
+                            3 -> funcion3()
+                            else -> funcion4()
+                        }
+
+                        iniciarAleatorio()
+                    }
+                }
+
+            })
+            thread.start()
+        }
+        hiloJuego()
     }
 }
