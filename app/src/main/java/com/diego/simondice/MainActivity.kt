e3087ab1124710ac721c2ede118d80b29a46dbdb
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-10-28 14:06:45 +0200

    Limpieza de código, optimización y modificación del diseño de los botones.

--- app/src/main/java/com/diego/simondice/MainActivity.kt
+++ app/src/main/java/com/diego/simondice/MainActivity.kt
@@ -1,48 +1,18 @@
 package com.diego.simondice
 
-import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.core.os.HandlerCompat.postDelayed
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.concurrent.thread
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val botonRojo: Button = findViewById(R.id.botonRojo)
-        botonRojo.setOnClickListener() {
-            Toast.makeText(applicationContext, "Rojo", Toast.LENGTH_SHORT).show()
-        }
-
-        val botonIzquierda: Button = findViewById(R.id.botonAzul)
-        botonIzquierda.setOnClickListener() {
-            Toast.makeText(applicationContext, "Azul", Toast.LENGTH_SHORT).show()
-        }
-
-        val botonAmarillo: Button = findViewById(R.id.botonAmarillo)
-        botonAmarillo.setOnClickListener() {
-            Toast.makeText(applicationContext, "Amarillo", Toast.LENGTH_SHORT).show()
-        }
-
-        val botonVerde: Button = findViewById(R.id.botonVerde)
-        botonVerde.setOnClickListener() {
-            Toast.makeText(applicationContext, "Verde", Toast.LENGTH_SHORT).show()
-        }
 
         val botonJugar: ImageButton = findViewById(R.id.botonJugar)
         botonJugar.setVisibility(View.VISIBLE)
@@ -53,73 +23,55 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun empezarJuego() {
-        val botonJugar: ImageButton = findViewById(R.id.botonJugar)
+
         val botonRojo: Button = findViewById(R.id.botonRojo)
-        val botonVerde: Button = findViewById(R.id.botonVerde)
         val botonAzul: Button = findViewById(R.id.botonAzul)
         val botonAmarillo: Button = findViewById(R.id.botonAmarillo)
+        val botonVerde: Button = findViewById(R.id.botonVerde)
 
-        val textoRondas: TextView = findViewById(R.id.textNRondas)
-        val PatronAResolver = Array(4) { "" }
+        botonRojo.isEnabled = false
+        botonAzul.isEnabled = false
+        botonAmarillo.isEnabled = false
+        botonVerde.isEnabled = false
 
-        CoroutineScope(Dispatchers.Default).launch {
-            for (i in 1..1) {
-                var posicionPatron = 0
-                textoRondas.text = i.toString()
-                for (e in 1..4) {
-                    val shuffled = (1..4).shuffled().last()
+        // Guardar el Patrón enviado
+        var patronAResolver=Array<String?>(4){null}
 
-                     suspend fun funcion1() {
-                        PatronAResolver.set(posicionPatron, "Rojo")
-                        botonRojo.setBackgroundColor(Color.parseColor("#8e1111"))
-                        delay(1000)
-                        botonRojo.setBackgroundColor(Color.parseColor("#ed1d1d"))
-                        posicionPatron++
-                        delay(500)
-                    }
 
-                    suspend fun funcion2() {
-                        PatronAResolver.set(posicionPatron, "Verde")
-                        botonVerde.setBackgroundColor(Color.parseColor("#1a691c"))
-                        delay(1000)
-                        botonVerde.setBackgroundColor(Color.parseColor("#2caf2f"))
-                        posicionPatron++
-                        delay(500)
-                    }
+        var posicionPatron = 0
 
-                    suspend fun funcion3() {
-                        PatronAResolver.set(posicionPatron, "Amarillo")
-                        botonAmarillo.setBackgroundColor(Color.parseColor("#89890b"))
-                        delay(1000)
-                        botonAmarillo.setBackgroundColor(Color.parseColor("#e5e513"))
-                        posicionPatron++
-                        delay(500)
-                    }
+        suspend fun suspendTaskColor(color: String, button: Button, colorID: String, Shadow_colorID: String) {
+            patronAResolver[posicionPatron] = color
+            button.setBackgroundColor(Color.parseColor(Shadow_colorID))
+            delay(1000)
+            button.setBackgroundColor(Color.parseColor(colorID))
+            posicionPatron++
+            delay(500)
+        }
 
-                    suspend fun funcion4() {
-                        PatronAResolver.set(posicionPatron, "Azul")
-                        botonAzul.setBackgroundColor(Color.parseColor("#0b4a8a"))
-                        delay(1000)
-                        botonAzul.setBackgroundColor(Color.parseColor("#137ce7"))
-                        posicionPatron++
-                        delay(500)
-                    }
+        val GenerarPatron = CoroutineScope(Dispatchers.IO).launch {
 
-                    suspend fun iniciarAleatorio() = when (shuffled) {
-                        1 -> funcion1()
-                        2 -> funcion2()
-                        3 -> funcion3()
-                        else -> funcion4()
-                    }
-                    iniciarAleatorio()
+            for (e in 1..4) {
+                val shuffled = (1..4).shuffled().last()
+
+                suspend fun iniciarAleatorio() = when (shuffled) {
+                    1 -> suspendTaskColor("Rojo",botonRojo,"#ed1d1d","#8e1111")
+                    2 -> suspendTaskColor("Verde",botonVerde,"#2caf2f","#1a691c")
+                    3 -> suspendTaskColor("Amarillo",botonAmarillo,"#e5e513","#89890b")
+                    else -> suspendTaskColor("Azul",botonAzul,"#137ce7","#0b4a8a")
                 }
 
-                println(PatronAResolver[0])
-                println(PatronAResolver[1])
-                println(PatronAResolver[2])
-                println(PatronAResolver[3])
-                posicionPatron = 0
+                println("Patron a Resolver:")
+                iniciarAleatorio()
             }
+
+            // Comprobar patrón enviado
+            println(patronAResolver[0])
+            println(patronAResolver[1])
+            println(patronAResolver[2])
+            println(patronAResolver[3])
+            // Resetea la posicion Array
+            posicionPatron = 0
         }
     }
 }
